d00c54c68fe8a64f0f6a3bf9b29d51584a4c42b8
zazazaza4 <6715414@gmail.com>
2022-09-06 10:48:32 +0300

    fix: to allow register and login

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -25,7 +25,7 @@ const AppRouter = () => {
     getMe();
   }, []);
 
-  return true ? (
+  return isAuth ? (
     <Routes>
       {privateRoutes.map(({ path, Component }) => {
         return <Route key={path} path={path} element={<Component />} />;
