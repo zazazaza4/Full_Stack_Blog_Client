7df86b98351c4c461184f49e1b15f1e5b7fee02c
zazazaza4 <6715414@gmail.com>
2022-09-15 10:23:59 +0300

    fix: loading in SinglePost page. add: media queries for SinglePost page

--- src/pages/SinglePost/SinglePost.jsx
+++ src/pages/SinglePost/SinglePost.jsx
@@ -18,7 +18,6 @@ import ReactMarkdown from 'react-markdown';
 import styles from './SinglePost.module.css';
 
 const SinglePost = () => {
-  const [loading, setLoading] = useState(true);
   const [post, setPost] = useState(null);
 
   const { popularPosts } = useSelector((state) => state.post);
@@ -32,11 +31,15 @@ const SinglePost = () => {
 
   useEffect(() => {
     fetchPost();
-    setLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.id]);
 
   if (!post) {
-    return;
+    return (
+      <div className={styles.spinner}>
+        <Spinner />
+      </div>
+    );
   }
 
   const {
@@ -53,75 +56,69 @@ const SinglePost = () => {
   return (
     <main className={styles.single}>
       <div className={styles.wrapper}>
-        {loading ? (
-          <div className={styles.spinner}>
-            <Spinner />
-          </div>
-        ) : (
-          <div className={styles.row}>
-            <motion.div
-              animate={{ opacity: [0, 0.5, 1], scale: [0, 1] }}
-              transition={{ ease: 'easeOut', duration: 1 }}
-              className={styles.left}
-            >
-              <article className={styles.article}>
-                <div className={styles.category}>{category}</div>
-                <h2 className={styles.title}>{title}</h2>
-                <div className={styles.date}>
-                  {format(`ddft MM yyyy`, new Date(createdAt))}
-                </div>
-                <div className={styles.img}>
-                  <img
-                    src={`${process.env.REACT_APP_API_URL}${
-                      photo || 'default.jpg'
-                    }`}
-                    alt=""
-                    className=""
-                  />
-                </div>
-                <ReactMarkdown children={text} />
-                <footer className={styles.footer}>
-                  <div className={styles.row}>
-                    <p className={styles.author}>
-                      Author:{' '}
-                      <Link className={styles.link} to={`/?userId=${author}`}>
-                        {username}
-                      </Link>
-                    </p>
-                    <p className={styles.views}>
-                      <img src={iconView} alt="Views:" /> {views}
-                    </p>
-                  </div>
-                </footer>
-              </article>
-              <div className={styles.comments}>
-                <h4 className={styles.h4}>COMMENTS</h4>
-                <ul className={styles.list_commnets}>
-                  {comments?.length > 0 ? (
-                    comments?.map((comment) => {
-                      return <Comment comment={comment} />;
-                    })
-                  ) : (
-                    <Announcement>No Comments yet</Announcement>
-                  )}
-                </ul>
+        <div className={styles.row}>
+          <motion.div
+            animate={{ opacity: [0, 0.5, 1], scale: [0, 1] }}
+            transition={{ ease: 'easeOut', duration: 1 }}
+            className={styles.left}
+          >
+            <article className={styles.article}>
+              <div className={styles.category}>{category}</div>
+              <h2 className={styles.title}>{title}</h2>
+              <div className={styles.date}>
+                {format(`ddft MM yyyy`, new Date(createdAt))}
               </div>
-              <div className={styles.comment__leave}>
-                <h4 className={styles.h4}>LEAVE A COMMENT</h4>
-                <form>
-                  <textarea
-                    placeholder="Messages"
-                    className={styles.textarea}
-                  ></textarea>
-                  <Button className={styles.button}>SEND MESSAGE</Button>
-                </form>
+              <div className={styles.img}>
+                <img
+                  src={`${process.env.REACT_APP_API_URL}${
+                    photo || 'default.jpg'
+                  }`}
+                  alt=""
+                  className=""
+                />
               </div>
-            </motion.div>
-            <div className={styles.right}>
-              <Widget title="POPULAR POSTS" list={popularPosts} />
+              <ReactMarkdown children={text} />
+              <footer className={styles.footer}>
+                <div className={styles.row_meta}>
+                  <p className={styles.author}>
+                    Author:{' '}
+                    <Link className={styles.link} to={`/?userId=${author}`}>
+                      {username}
+                    </Link>
+                  </p>
+                  <p className={styles.views}>
+                    <img src={iconView} alt="Views:" /> {views}
+                  </p>
+                </div>
+              </footer>
+            </article>
+            <div className={styles.comments}>
+              <h4 className={styles.h4}>COMMENTS</h4>
+              <ul className={styles.list_commnets}>
+                {comments?.length > 0 ? (
+                  comments?.map((comment) => {
+                    return <Comment comment={comment} />;
+                  })
+                ) : (
+                  <Announcement>No Comments yet</Announcement>
+                )}
+              </ul>
             </div>
+            <div className={styles.comment__leave}>
+              <h4 className={styles.h4}>LEAVE A COMMENT</h4>
+              <form>
+                <textarea
+                  placeholder="Messages"
+                  className={styles.textarea}
+                ></textarea>
+                <Button className={styles.button}>SEND MESSAGE</Button>
+              </form>
+            </div>
+          </motion.div>
+          <div className={styles.right}>
+            <Widget title="POPULAR POSTS" list={popularPosts} />
           </div>
-        )}
+        </div>
       </div>
     </main>
   );
