d9c41f2fb7db32075c52adfaeb82a6b8a1306866
Yaroslav <6715414@gmail.com>
2023-01-17 12:37:49 +0200

    rewrote AppRouter in ts

--- src/pages/AppRouter.tsx
+++ src/pages/AppRouter.tsx
@@ -1,32 +1,39 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { privateRoutes, publicRoutes } from '../routes/routes';
-import { HOME_ROUTE, LOGIN_ROUTE } from '../utils/consts';
-import { useDispatch, useSelector } from 'react-redux';
-import { checkIsAuth, logIn } from '../redux/slices/auth/authSlice';
-import { Suspense, useEffect } from 'react';
-import axios from '../utils/axios';
-import { Spinner } from '../components';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { privateRoutes, publicRoutes } from "../config/routes";
+import { HOME_ROUTE, LOGIN_ROUTE } from "../utils/consts";
+import { useDispatch, useSelector } from "react-redux";
+import { checkIsAuth, logIn } from "../redux/slices/auth/authSlice";
+import { Suspense, useEffect, FC, useCallback } from "react";
+import axios from "../utils/axios";
+import { Spinner } from "../components";
 
-import styles from './AppRouter.module.css';
+import styles from "./AppRouter.module.css";
+import { IUser } from "../types/User.interface";
 
-const AppRouter = () => {
+interface GetAuthsResponse {
+  message: string;
+  user?: IUser;
+  token?: string;
+}
+
+const AppRouter: FC = () => {
   const isAuth = useSelector(checkIsAuth);
 
   const dispatch = useDispatch();
 
-  const getMe = async () => {
+  const getMe = useCallback(async () => {
     try {
-      const { data } = await axios.get('auth/me');
+      const { data } = await axios.get<GetAuthsResponse>("auth/me");
       dispatch(logIn(data));
     } catch (error) {
       console.log(error);
     }
-  };
+  }, [dispatch]);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     getMe();
-  }, []);
+  }, [getMe]);
 
   return (
     <Suspense
@@ -50,7 +57,7 @@ const AppRouter = () => {
           })}
           <Route path="*" element={<Navigate replace to={LOGIN_ROUTE} />} />
         </Routes>
-      )}{' '}
+      )}{" "}
     </Suspense>
   );
 };
